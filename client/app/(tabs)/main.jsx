4042adf6ae68b189bccf379372b97ae1ef93ce72
shanylus <shanilos100@gmail.com>
2025-08-05 17:24:24 +0300

    main

--- client/app/(tabs)/main.jsx
+++ client/app/(tabs)/main.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect, useRef} from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import {
   View,
   Text,
@@ -7,14 +7,14 @@ import {
   TouchableOpacity,
   Dimensions,
   ImageBackground,
-  Animated
+  Animated,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
 import { router } from "expo-router";
 const { width, height } = Dimensions.get("window");
-import {DataContext} from "../DataContextProvider" 
-import { Shadow } from 'react-native-shadow-2';
+import { DataContext } from "../DataContextProvider";
+import { Shadow } from "react-native-shadow-2";
 
 export default function VideoCallStartScreen() {
   const navigation = useNavigation();
@@ -23,54 +23,56 @@ export default function VideoCallStartScreen() {
   console.log("user:", user);
 
   const translateY = useRef(new Animated.Value(0)).current;
-   useEffect(() => {
-      Animated.loop(
-        Animated.sequence([
-          Animated.timing(translateY, {
-            toValue: -10,
-            duration: 1500,
-            useNativeDriver: true,
-          }),
-          Animated.timing(translateY, {
-            toValue: 0,
-            duration: 1500,
-            useNativeDriver: true,
-          }),
-        ])
-      ).start();
-    }, []);
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(translateY, {
+          toValue: -10,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(translateY, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
 
   // const FloatingBubbleButton = ({ onPress }) => {
 
-   
   // };
 
   return (
-    <ImageBackground
-      source={require("../../assets/images/design.png")}
-      style={styles.backgroundImage}
-      resizeMode="cover"
+    <LinearGradient
+      colors={["rgba(106,13,173,0.7)", "rgba(209,71,163,0.7)"]}
+      style={styles.overlay}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
     >
-      <LinearGradient
-        colors={["rgba(106,13,173,0.7)", "rgba(209,71,163,0.7)"]}
-        style={styles.overlay}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-      >
+      {/* SECTION עליון */}
+      <View style={styles.topSection}>
         <View style={styles.header}>
-          <View style = {{alignItems:"center", flexDirection: "column", paddingTop: 16,}}>
+          <View
+            style={{
+              alignItems: "center",
+              flexDirection: "column",
+              paddingTop: 16,
+            }}
+          >
             <View style={styles.logoCircle}>
-            <Image
-              source={require("../../assets/images/AppLogo.png")}
-              style={styles.logoImage}
-              resizeMode="contain"
-            />
+              <Image
+                source={require("../../assets/images/AppLogo.png")}
+                style={styles.logoImage}
+                resizeMode="contain"
+              />
             </View>
             <Text style={styles.logo}>Luvio</Text>
           </View>
           <TouchableOpacity
             onPress={() => navigation.navigate("profile")}
-            style = {{paddingRight: 10}}
+            style={{ paddingRight: 10 }}
           >
             <Image
               source={{
@@ -80,52 +82,108 @@ export default function VideoCallStartScreen() {
             />
           </TouchableOpacity>
         </View>
-        
 
-        {/* תמונת פרופיל */}
         <Image
-          source={user && user.profileImage ? { uri: user.profileImage} :{uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"}}
+          source={
+            user && user.profileImage
+              ? { uri: user.profileImage }
+              : {
+                  uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+                }
+          }
           style={styles.avatar}
         />
 
-         <Text style={styles.title}>Ready to Connect</Text>
+        <Text>
+          <Text
+            style={{
+              fontSize: 26,
+              color: "#fff",
+              fontFamily: "Prompt-Thin",
+            }}
+          >
+            Hello👋{" "}
+          </Text>
+          <Text
+            style={{
+              fontSize: 22,
+              color: "#fff",
+              fontFamily: "Prompt-SemiBold",
+            }}
+          >
+          {user.userName}
+          </Text>
+        </Text>
+        {/* <Text style={styles.bubbleText}>Nice to see you!</Text> */}
+      </View>
+
+      <ImageBackground
+        source={require("../../assets/images/design.png")}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        {/* SECTION תחתון */}
+        <View style={styles.bottomSection}>
+          <Text style={styles.title}>Ready to Connect</Text>
           <Text style={styles.subtitle}>Tap to start your video chat</Text>
 
-          {/* כפתור עגול עם אייקון מצלמה */}
           <View style={styles.container}>
             <View style={styles.shadowEllipse} />
             <Animated.View style={{ transform: [{ translateY }] }}>
-            {/* Bubble */}
-              <TouchableOpacity style={styles.bubble}  onPress={() => router.navigate("/videoCall")}>
+              <TouchableOpacity
+                style={styles.bubble}
+                onPress={() => router.navigate("/videoCall")}
+              >
                 <Text style={styles.bubbleText}>Start Call</Text>
               </TouchableOpacity>
             </Animated.View>
           </View>
-      </LinearGradient>
-    </ImageBackground>
+        </View>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
+  topSection: {
+    // flex: 1, // הקטנתי קצת את החלק העליון
+    width: "100%",
+    backgroundColor: "hsla(0, 11.10%, 7.10%, 0.45)",
+    alignItems: "center",
+    paddingBottom: 1,
+    backgroundColor: "rgba(0,0,0,0.15)",
+    borderRadius: 29,
+    shadowColor: "#000",
+    shadowOpacity: 0.08,
+    shadowRadius: 12,
+    shadowOffset: { width: 0, height: 4 },
   },
+
+  bottomSection: {
+    flex: 90.5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: "100%",
+    paddingTop: 30,
+    paddingHorizontal: 20,
+  },
+
   overlay: {
     flex: 1,
     alignItems: "center",
   },
   header: {
-    width:"100%",
+    width: "100%",
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",   
+    justifyContent: "space-between",
     paddingHorizontal: 16,
     // backgroundColor: "#DA58B7",
   },
   logo: {
-      fontSize: 22,
-      fontWeight: "bold",
-      color: "#DA58B7",
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#DA58B7",
   },
   logoCircle: {
     width: 40,
@@ -139,7 +197,7 @@ const styles = StyleSheet.create({
     elevation: 12,
     position: "relative",
   },
-  logoImage: { 
+  logoImage: {
     width: 40,
     height: 40,
   },
@@ -149,11 +207,10 @@ const styles = StyleSheet.create({
     tintColor: "#fff",
   },
   avatar: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    borderWidth: 2,
-    borderColor: "#fff",
+    width: 140,
+    height: 140,
+    borderRadius: 50,
+    shadowColor: "#DA58B7",
     marginBottom: 20,
   },
   card: {
@@ -191,35 +248,27 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 5 },
   },
   bubble: {
-    width: 170,
-    height: 170,
-    borderRadius: 100,
-    backgroundColor: 'rgba(180, 26, 103, 0.17)', // Transparent pink
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: 200,
+    height: 140,
+    borderRadius: 40,
+    backgroundColor: "rgba(255,255,255,0.15)",
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 2,
-    borderColor: "rgba(180, 26, 103, 0.29))",
+    borderColor: "rgba(255,255,255,0.15)",
   },
   bubbleText: {
-    color: '#ffffffda',
+    color: "#ffffffda",
     fontSize: 20,
     fontFamily: "Prompt-SemiBold",
   },
-   shadowEllipse: {
-    position: 'absolute',
-    bottom: -30, // pushes it below the bubble
-    width: 100,
-    height: 50,
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    borderRadius: 30,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0,
-    shadowRadius: 1,
-    elevation: 18,
+  container: {
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
-   container: {
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
   },
 });
